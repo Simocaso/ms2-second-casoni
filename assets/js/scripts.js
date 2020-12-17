6462799289c46ab0a7e21e65b772ad3525d2280c
Simocaso <simonecasoni97@gmail.com>
2020-12-17 01:43:20 +0000

    finished function to show the first player when to play

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,10 @@
 function firstMove() {
     document.turn = "a";
+
+    showGameMsg( document.turn + " starts");
+}
+
+function showGameMsg(message) {
+    document.getElementById("game-msg").innerText = message;
 }
 
